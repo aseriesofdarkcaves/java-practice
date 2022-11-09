cb452752fcd209ce65c56e3ed892e5ee62121ead
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2022-11-09 14:01:37 +0100

    Update standard-streams module
    - make StandardStreams class easier to read and follow

--- standard-streams/src/main/java/com/asodc/java/standardstreams/StandardStreams.java
+++ standard-streams/src/main/java/com/asodc/java/standardstreams/StandardStreams.java
@@ -7,10 +7,15 @@ import java.io.PrintStream;
 
 public class StandardStreams {
     public static void main(String... args) {
-        System.out.println("BEFORE!");
+        // by default, standard output is usually the console
+        System.out.println("THIS IS THE STANDARD OUTPUT STREAM, WHICH IS USUALLY THE CONSOLE!");
+
+        // redirect standard output to something else
         System.setOut(new PrintStream(new ByteArrayOutputStream()));
-        System.out.println("AFTER!");
+        System.out.println("YOU WON'T SEE THIS IN THE CONSOLE AS STANDARD OUTPUT HAS BEEN REDIRECTED!");
+
+        // redirect standard output again
         System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
-        System.out.println("ANOTHER AFTER!");
+        System.out.println("YOU SHOULD BE ABLE TO SEE THIS IN THE CONSOLE!");
     }
 }
