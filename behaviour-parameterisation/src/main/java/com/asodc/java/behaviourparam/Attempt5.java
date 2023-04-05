b7e447f80bbebcb0a0d770c3ec360e2c80aab45d
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2023-04-05 11:05:53 +0200

    Add Attempt5 & Attempt6 - Add Attempt5 illustrating anonymous class - Add Attempt6 illustrating lambda expression

---
+++ behaviour-parameterisation/src/main/java/com/asodc/java/behaviourparam/Attempt5.java
@@ -0,0 +1,44 @@
+package com.asodc.java.behaviourparam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Attempt5 {
+    public static void main(String[] args) {
+        // Generate apple inventory
+        List<Apple> inventory = AppleUtil.generateApples(10);
+
+        // Print inventory
+        AppleUtil.printAppleList("INVENTORY", inventory);
+
+        // Print filtered inventory using anonymous class - ignore IDE saying you can convert to lambda for now
+        List<Apple> redApples = filterApples(inventory, new ApplePredicate() {
+            @Override
+            public boolean test(Apple apple) {
+                return Colour.RED.equals(apple.getColour());
+            }
+        });
+        AppleUtil.printAppleList("RED APPLES", redApples);
+    }
+
+    /**
+     * The behaviour of the filterApples method depends on code you pass to it via the ApplePredicate object.
+     * You've parameterised the behaviour of the filterApples method!
+     * Behaviour parameterisation is great as it enables you to separate the logic of iterating over the collection to
+     * filter and the behaviour to apply to each element of the collection. As a consequence, you can reuse the same
+     * method and give it different behaviors to achieve different things.
+     *
+     * @param inventory the apple inventory to be filtered
+     * @param predicate the predicate to apply to the filter logic
+     * @return a list containing the filtered apple inventory
+     */
+    public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate predicate) {
+        List<Apple> filteredInventory = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (predicate.test(apple)) {
+                filteredInventory.add(apple);
+            }
+        }
+        return filteredInventory;
+    }
+}
